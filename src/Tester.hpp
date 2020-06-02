f246837eded786c87f5c290ca0cd5157be304092
Alexander Epaneshnikov <aarnaarn2@gmail.com>
2020-06-03 00:25:08 +0300

    remove "using namespace std;"
    make code cleaner

--- src/Tester.hpp
+++ src/Tester.hpp
@@ -8,28 +8,26 @@
 #include "Attempt.hpp"
 #include "CSVAdapter.hpp"
 
-using namespace std;
-
 class Tester
 {
 private:
     /* data */
-    static const string INSTRUCTION;
-    static const string RESULT1;
-    static const string RESULT2;
-    static const string RESULT3;
-    static const string RESULT4;
-    
+    static const std::string INSTRUCTION;
+    static const std::string RESULT1;
+    static const std::string RESULT2;
+    static const std::string RESULT3;
+    static const std::string RESULT4;
+
     std::unique_ptr<CSVStorage<Person>> storage;
-    Attempt* currentAttempt = nullptr; 
-    
+    Attempt* currentAttempt = nullptr;
+
 public:
     Tester(/* args */);
     ~Tester();
     Attempt* AddAttempt(const Person& person);
     Attempt* GetAttempt();
-    static const string& GetInstruction();
-    static const string& DecodeResult(uint8_t result);
+    static const std::string& GetInstruction();
+    static const std::string& DecodeResult(uint8_t result);
     void SaveCurrentAttempt();
 };
 
